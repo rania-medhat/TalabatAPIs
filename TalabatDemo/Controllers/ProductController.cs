62d1b01b5239aa7e8495e023f0bb45b9a72830e6
rania-medhat <raniamedhat59@gmail.com>
2025-10-24 23:24:36 +0300

    talabat apis

---
+++ TalabatDemo/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TalabatDemo.Models;
+
+namespace TalabatDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase //baseurl/api/Product
+    {
+        [HttpGet("{id}")]//Get : BaseURL/api/Product?id=6
+        public ActionResult<Product> Get(int id) 
+        {
+         return new Product() { Id = id };
+        }
+
+        [HttpGet]//Get : BaseURL/api/Product
+        public ActionResult<Product> GetAll(int id)
+        {
+            return new Product() { Id = 20 };
+        }
+
+        [HttpPost]
+        public ActionResult<Product> AddProduct(Product product)
+        {
+            return new Product();
+        }
+
+        [HttpPost("brand")]
+        public ActionResult<Product> AddBrand(Product product)
+        {
+            return new Product();
+        }
+
+        [HttpPut]
+        public ActionResult<Product> UpdateProduct(Product product)
+        {
+            return new Product();
+        }
+
+        [HttpDelete]
+        public ActionResult<Product> DeleteProduct(Product product)
+        {
+            return new Product();
+        }
+    }
+}
---
+++ TalabatDemo/Program.cs
@@ -0,0 +1,36 @@
+
+namespace TalabatDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            #region add services to the container
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(); 
+            #endregion
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
